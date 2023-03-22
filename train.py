00e5a17bae72e557e310eb26b9b4b7b075525d55
Prabhupad Pradhan <prabhupad26@gmail.com>
2023-03-22 11:05:55 +0100

    manual run changes : delta

--- train.py
+++ train.py
@@ -627,8 +627,7 @@ def run(**kwargs):
     opt = parse_opt(True)
     for k, v in kwargs.items():
         setattr(opt, k, v)
-    main(opt)
-    return opt
+    return main(opt)
 
 
 if __name__ == "__main__":
